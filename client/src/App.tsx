15f9c0573598f302ac1a3c22f5dc34ae8cecf3e5
Bayethe Lungah <Bayethelungah@gmail.com>
2023-02-15 21:59:51 -0500

    Added new about page route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Home from "./Home";
 import Study from "./Study";
+import About from "./About";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/study" element={<Study />} />
+        <Route path="/About" element={<About />} />
       </Routes>
     </Router>
   );
